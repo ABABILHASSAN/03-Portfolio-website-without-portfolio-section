68c75429e1a0d9e04c1e331fc10f2a3c2aac488f
ABABIL HASSAN <88208556+ABABILHASSAN@users.noreply.github.com>
2025-03-25 10:42:30 +0600

    03-Portfolio-website-without-portfolio-section
    03-Portfolio-website-without-portfolio-section

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Header Navbar Starts From Here
+
+const list = document.querySelectorAll('.list');
+function activeLink() {
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+item.addEventListener('click',activeLink));
+
+// Home Section Starts From Here 
+
+var swiper = new Swiper(".home-slider" , {
+    spaceBetween: 420,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: "swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
+
